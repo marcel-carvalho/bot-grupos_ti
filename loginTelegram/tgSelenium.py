190695b7849f541ff30837fa083e6aa890e42589
roliveira <raphaelbrf@gmail.com>
2017-06-14 17:54:29 -0300

    função clicar ok para entrar no grupo

--- loginTelegram/tgSelenium.py
+++ loginTelegram/tgSelenium.py
@@ -11,8 +11,8 @@ browser = webdriver.Firefox()
 browser.get('https://web.telegram.org/#/login')
 login_telegram(browser, phone)
 
-print('sleep time wait 20 seconds')
-sleep(20)
+print('sleep time wait 10 seconds')
+sleep(10)
 print('sleep time over')
 
 new_tab(browser)
@@ -20,9 +20,17 @@ browser.get('https://www.google.com.br/')
 
 new_tab(browser)
 browser.get('https://web.telegram.org/#/im?tgaddr=tg%3A%2F%2Fjoin%3Finvite%3DAAAAAEDrakjDNoJR0vtulw')
-box = WebDriverWait(browser, 10).until(lambda browser: browser.find_elements_by_tag_name('button'))
-print(box)
-box.click()
+sleep(10)
+
+#browser.find_element_by_class_name('modal fade confirm_modal_window in')
+#browser.find_element_by_class_name('modal-dialog')
+#browser.find_element_by_class_name('md_simple_modal_footer')
+box_group = WebDriverWait(browser, 10).until(lambda browser: browser.find_elements_by_tag_name('button'))
+print(box_group)
+box_group[3].click()
+
+
+#box.click()
 
 #browser.get('https://web.telegram.org/#/im?tgaddr=tg%3A%2F%2Fjoin%3Finvite%3DAAAAAEDrakjDNoJR0vtulw')
 #browser.get('https://t.me/joinchat/AAAAAEDrakjDNoJR0vtulw')
