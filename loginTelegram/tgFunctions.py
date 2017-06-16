fbe72b094c59d1ea62e084dca797f22fb8f6c9fb
roliveira <raphaelbrf@gmail.com>
2017-06-16 15:10:07 -0300

    up

--- loginTelegram/tgFunctions.py
+++ loginTelegram/tgFunctions.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
+from time import sleep
 
 def login_telegram(browser,phone_num):
     '''function with steps to login in telegram web'''
@@ -14,6 +15,8 @@ def login_telegram(browser,phone_num):
     #send code that you receive in your telegram
     code = WebDriverWait(browser, 10).until(lambda browser: browser.find_element_by_name('phone_code'))
     code.send_keys(input('Phone Code: '))
+    #loading page
+    print('sleep time: 10 seconds'), sleep(10), print('sleep time over')
 
 
 def new_tab(browser):
--- loginTelegram/tgSelenium.py
+++ loginTelegram/tgSelenium.py
@@ -6,22 +6,26 @@ from tgFunctions import *
 
 #colocar o numero do telefone com DDD ex: 021 7777-7777
 phone = input('Phone number with DDD: ')
-
+phone = '21995181636'
+#login no telegram web
 browser = webdriver.Firefox()
 browser.get('https://web.telegram.org/#/login')
 login_telegram(browser, phone)
 
-print('sleep time: 10 seconds')
-sleep(10)
-print('sleep time over')
-
 # entrando no grupo
 new_tab(browser)
 browser.get('https://web.telegram.org/#/im?tgaddr=tg%3A%2F%2Fjoin%3Finvite%3DAAAAAEDrakjDNoJR0vtulw')
-print('sleep time: 5 seconds')
-sleep(5)
+print('sleep time: 3 seconds'), sleep(3)
 box_group = WebDriverWait(browser, 10).until(lambda browser: browser.find_elements_by_tag_name('button'))
 box_group[2].click()
+
+# coletando dados do grupo
+browser.find_element_by_class_name('tg_head_btn').click()
+name_group = browser.find_element_by_class_name('peer_modal_profile').text
+num_users = browser.find_element_by_class_name('peer_modal_profile_description').text
+print('Nome do Grupo: {0}\n Numero de participantes: {1}' .format(name_group, num_users))
+
+#saindo do grupo
 
 
 #browser.get('https://web.telegram.org/#/im?tgaddr=tg%3A%2F%2Fjoin%3Finvite%3DAAAAAEDrakjDNoJR0vtulw')
