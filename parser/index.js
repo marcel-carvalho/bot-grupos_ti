b49c0666cb366bbc93e5ed77c9b79003a0b46d5d
Pablo R. Dinella <pablo.dinella@gmail.com>
2017-07-17 14:39:28 -0300

    Melhorias no parser

--- parser/index.js
+++ parser/index.js
@@ -7,15 +7,15 @@ const OUTPUT = path.join(__dirname, 'output/entries.json');
 
 function getEntries(html) {
   const $ = load(html);
-  const entries = $('.im_message_text').map((i, el) => {
-    const tags = $(el).find('[href^="tg://search_hashtag"]').text();
-    const desc = $(el).clone().children().remove().end().text().trim().replace(/^-\s/, '');
-    const link = $(el).find('[href^="tg://join"]').text();
+  const entries = $('.im_message_text, .im_message_photo_caption').map((i, el) => {
+    let tags = $(el).find('[href^="tg://search_hashtag"]').text();
+    let desc = $(el).clone().children(':not(br):not([href^="tg://search_hashtag"])').remove().end().find('br').replaceWith(' ').end().text().trim().replace(/^-\s/, '').replace(/\s\s+/g, '\n');
+    let link = $(el).find('[href^="tg://join"],[href^="tg://resolve"],[href^="https://www.telegram.me/"]').text();
     return {tags, desc, link};
   }).get()
   console.log(entries);
   console.log(`${entries.length} grupos/canais encontrados`);
-  return JSON.stringify(entries);
+  return JSON.stringify(entries, null, '  ');
 }
 
 function parse(path) {
